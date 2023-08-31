13dfc79ef2364b043a4687ec3214d5d9da04c477
Mukhammadaliti <mukhammadaliti02@gmail.com>
2023-08-31 19:32:12 +0600

    svg icons is save

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -11,36 +11,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Image.asset(
-          'assets/images/logo.png',
-          color: Colors.white,
-          width: 200,
-        ),
-        actions: [
-          InkWell(
-            onTap: () {
-              print("Wishlist");
-            },
-            child: Image.asset(
-              'assets/images/Vector.png',
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Text(
-                'Explore',
-              ),
-            ],
-          ),
-        ),
-      ),
+      
+    body: Container(child: Text("HOME PAGE"),),
     );
   }
 }
